17244e5de59c883b3454d8b0996f3b6924311ca9
PashkevichEvgeny <pash.ev@ya.ru>
2023-03-11 23:32:10 +0800

    add reading user input

--- Program.cs
+++ Program.cs
@@ -1,13 +1,14 @@
 ﻿string[] wordLessOrEqualThree(string userText)
 {
+    int wordSize = 3;
     string[] array = userText.Split(' ', StringSplitOptions.RemoveEmptyEntries);
     int amountWords = 0;
-    foreach (var item in array) if (item.Length <= 0) amountWords += 1;
+    foreach (var item in array) if (item.Length <= wordSize) amountWords += 1;
     string[] new_array = new string[amountWords];
     amountWords = 0;
     for (int i = 0; i < array.Length; i++)
     {
-        if (array[i].Length <= 3)
+        if (array[i].Length <= wordSize)
         {
             new_array[amountWords] = array[i];
             amountWords += 1;
@@ -15,3 +16,8 @@
     }
     return new_array;
 }
+
+Console.Write("Напишите несколько слов: ");
+string userString = Console.ReadLine()!;
+string[] testArray = wordLessOrEqualThree(userString);
+Console.WriteLine($"[\"{string.Join("\", \"", testArray)}\"]");
