d2b064ac8f3d597aaebb2806a2eee616c56bc3ed
PashkevichEvgeny <pash.ev@ya.ru>
2023-03-11 22:59:50 +0800

    add method for writing to an array of words of the desired length

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿string[] wordLessOrEqualThree(string userText)
+{
+    string[] array = userText.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    int amountWords = 0;
+    foreach (var item in array) if (item.Length <= 0) amountWords += 1;
+    string[] new_array = new string[amountWords];
+    amountWords = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            new_array[amountWords] = array[i];
+            amountWords += 1;
+        }
+    }
+    return new_array;
+}
